f2e0a7893460dc3220a119135ce7fb12de8549a0
weliton <welingtonzsilva@gmail.com>
2022-02-11 02:50:48 -0300

    controller para usuario concluido]

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,40 @@
 
     namespace Weliton\ApiShine\Controller;
 
-class UserController
-{
+use PDO;
+use Weliton\ApiShine\Helper\RenderHtml;
+use Weliton\ApiShine\Infra\Connection;
+
+class UserController implements InterfaceRequest
+{   
+    use RenderHtml;
+
+    private PDO $pdo;
+    private string $table;
+    private string $id;
+
+    public function __construct(string $table, int $id)
+    {
+        $this->table = $table;
+        $this->pdo = new Connection();
+    }
     
+    public function request(): void
+    {
+        $sql = "SELECT * FROM {$this->table} WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':id',$this->id);
+        $stmt->execute();
+
+        if($stmt->rowCount() > 0){
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            echo $this->html('teste/teste.php',[
+                'usuario' => $user,
+                'titulo' => 'Api Shine'
+            ]);
+        }else{
+            throw new \Exception("nenhum usuario encontrado !");
+        }
+
+    }
 }
