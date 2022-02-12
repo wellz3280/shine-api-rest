75aca24a3cfab98fae806767f38a0702d9dd6e8c
weliton <welingtonzsilva@gmail.com>
2022-02-12 11:22:13 -0300

    adicionada rota para home.php

--- config/route.php
+++ config/route.php
@@ -1,9 +1,11 @@
 <?php
 
+use Weliton\ApiShine\Controller\HomeController;
 use Weliton\ApiShine\Controller\TesteController;
 use Weliton\ApiShine\Controller\UserController;
 
 return[
+        '/'=> HomeController::class,
         '/teste' => TesteController::class,
         '/user' => UserController::class
     ];
