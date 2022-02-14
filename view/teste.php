a40a44f11cbeb37e230b0beb8c0bb9e20c55873f
weliton <welingtonzsilva@gmail.com>
2022-02-14 02:46:31 -0300

    testando aplicação

--- view/teste.php
+++ view/teste.php
@@ -1,11 +1,4 @@
 <?php
 
-   // header('Content-Type: application/json');
-   // echo $userJson;
-
-   echo $tituloPagina;
-
-   foreach($table as $tables){
-      echo $tables['Tables_in_testShine']."<br>";
-   
-   }
+    header('Content-Type: application/json');
+   echo $userJson;
