841e2aed2f0d19e75b9871012b3388024e5469bd
weliton <welingtonzsilva@gmail.com>
2022-02-18 02:20:02 -0300

    controller para formulario de imserção ao banco implementado

---
+++ src/Controller/FormInserirLinhaController.php
@@ -0,0 +1,30 @@
+<?php
+    namespace Weliton\ApiShine\Controller;
+
+use Weliton\ApiShine\Helper\RenderHtml;
+use Weliton\ApiShine\Shine\Infra\Persistance\Connection;
+use Weliton\ApiShine\Shine\Infra\Persistance\Select;
+
+class FormInserirLinhaController implements InterfaceController
+{
+    use RenderHtml;
+    public function request(): void
+    {
+        $table = filter_input(INPUT_GET,'table');
+
+        $conn = Connection::startConn();
+        $query = new Select($conn);
+
+        $result = $query->write("SHOW COLUMNS FROM {$table}");
+        $datas = [];
+        foreach($result as $data){
+            $datas[]= $data['Field'];
+            unset($datas[0]);
+        }
+        
+        echo $this->html('/site/formInserirLinha.php',[
+            "tituloPagina" => $table,
+            "tableField" => $datas
+        ]);
+    }
+}
