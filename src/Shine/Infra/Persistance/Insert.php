c09eff9ad2a67188b2bdce40466020b4a5032b9e
weliton <welingtonzsilva@gmail.com>
2022-02-22 01:44:25 -0300

    objeto agora devolve um boleano

--- src/Shine/Infra/Persistance/Insert.php
+++ src/Shine/Infra/Persistance/Insert.php
@@ -2,6 +2,7 @@
     namespace Weliton\ApiShine\Shine\Infra\Persistance;
 
 use PDO;
+use PDOException;
 use Weliton\ApiShine\Shine\Helper\TakeByIndex;
 
 class Insert extends Select
@@ -19,6 +20,7 @@ class Insert extends Select
 
     private function getFieldsStr(string $table):string
     {
+        // pega as colunas da tabela e transforma em string
         $filds = [];
         $query = $this->write("SHOW COLUMNS FROM {$table}");
         foreach($query as $data){
@@ -31,6 +33,7 @@ class Insert extends Select
 
     private function getParamStr(string $data):string
     {
+        // preparando a string para servir de parâmetro
         $aData = explode(",",$data);
         $param = [];
         
@@ -43,7 +46,7 @@ class Insert extends Select
 
     private function getParamArray(string $data):array
     {
-        // pegandando 
+        // preparando o array para servir de parâmetro
         $aData = explode(",",$data);
         $param = [];
         
@@ -53,42 +56,47 @@ class Insert extends Select
         
         return $param;
     }
-
-    public function doIt(string $table,array $data):void
+    
+    public function shine(string $table,array $data):bool
     {
+        try{
+            $query = "INSERT INTO {$table} (";
+            
+            $filds = $this->getFieldsStr($table);
+            
+            $arrayFields = $this->getParamArray($filds);
+            
+            $values = " VALUES ({$this->getParamStr($filds)});";
+            
+            $sql = $query.$filds.") ".$values;
+            
+            $result = $this->pdo->prepare($sql);
+            
+            foreach(array_combine($arrayFields,$data)as $p => $v){
+                
+                if(is_string($v)){
+                    $result->bindValue("{$p}",$v,PDO::PARAM_STR);
+                }
+                
+                if(is_int($v)){
+                    $result->bindValue("{$p}",intval($v),PDO::PARAM_INT);
+                }
+                
+                if(is_float($v)){
+                    $result->bindValue("{$p}",floatval($v));
+                }
+                
+                if(is_double($v)){
+                    $result->bindValue("{$p}",doubleval($v));
+                }
+            }
 
-        $query = "INSERT INTO {$table} (";
+            $result->execute();
+           
+            return true;
         
-        $filds = $this->getFieldsStr($table);
-        
-        $arrayFields = $this->getParamArray($filds);
-        
-        $values = " VALUES ({$this->getParamStr($filds)});";
-        
-        $sql = $query.$filds.") ".$values;
-        
-        $result = $this->pdo->prepare($sql);
-        
-        foreach(array_combine($arrayFields,$data)as $p => $v){
-            
-            if(is_string($v)){
-                $result->bindValue("{$p}",$v,PDO::PARAM_STR);
-            }
-            
-            if(is_int($v)){
-                $result->bindValue("{$p}",intval($v),PDO::PARAM_INT);
-            }
-            
-            if(is_float($v)){
-                $result->bindValue("{$p}",floatval($v));
-            }
-            
-            if(is_double($v)){
-                $result->bindValue("{$p}",doubleval($v));
-            }
+        }catch(PDOException $e){
+            echo "ERROR: ". $e->getMessage();
         }
-
-        $result->execute();
-
     }   
 }
