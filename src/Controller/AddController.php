3cf42127b6c91760cee85814c30bc576f28b1359
weliton <welingtonzsilva@gmail.com>
2022-02-22 01:41:47 -0300

    controller adiciona linha no banco de dados implemetado

---
+++ src/Controller/AddController.php
@@ -0,0 +1,30 @@
+<?php
+    namespace Weliton\ApiShine\Controller;
+
+use Weliton\ApiShine\Helper\FlashMensage;
+
+use Weliton\ApiShine\Shine\Helper\UrlParam;
+use Weliton\ApiShine\Shine\Infra\Persistance\Connection;
+use Weliton\ApiShine\Shine\Infra\Persistance\Insert;
+
+class AddController implements InterfaceController
+{
+    
+    use FlashMensage;
+    use UrlParam;
+    
+    public function request(): void
+    {
+        $pdo = Connection::startConn();
+        $table = filter_input(INPUT_POST,'table');
+        $values = $this->getParamPost($table,$pdo);
+        unset($values[0]);
+        $insert = new Insert($pdo);
+        $result = $insert->shine($table,$values);
+        
+        if($result){
+            header("Location:/view?table={$table}");
+            $this->showMensage('success','Adidionado com Sucesso');
+        }
+    }
+}
