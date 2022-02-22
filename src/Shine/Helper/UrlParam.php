9d8b42042883f5a68cc33d6603259e87f2816127
weliton <welingtonzsilva@gmail.com>
2022-02-22 01:35:11 -0300

    trait pega os parametros da url e devolve um array

---
+++ src/Shine/Helper/UrlParam.php
@@ -0,0 +1,24 @@
+<?php
+    namespace Weliton\ApiShine\Shine\Helper;
+
+use PDO;
+use Weliton\ApiShine\Shine\Infra\Persistance\Select;
+
+trait UrlParam 
+{
+   
+
+    public function getParamPost(string $table, PDO $pdo):array
+    {
+        $query = new Select($pdo);
+        $result = $query->write("SHOW COLUMNS FROM {$table} ");
+        
+        $values = [];
+        foreach($result as $data){
+           $values[] = filter_input(INPUT_POST,$data['Field']);
+        } 
+        
+        return $values;
+    
+    }   
+}
