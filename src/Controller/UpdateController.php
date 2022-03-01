58e9c6c5cccce1f639deb77e02c4d0208b39ece6
weliton <welingtonzsilva@gmail.com>
2022-02-28 23:57:15 -0300

    update implementado

--- src/Controller/UpdateController.php
+++ src/Controller/UpdateController.php
@@ -2,10 +2,35 @@
 
     namespace Weliton\ApiShine\Controller;
 
+use Weliton\ApiShine\Helper\FlashMensage;
+use Weliton\ApiShine\Shine\Helper\UrlParam;
+use Weliton\ApiShine\Shine\Infra\Persistance\Connection;
+use Weliton\ApiShine\Shine\Infra\Persistance\Update;
+
 class UpdateController implements InterfaceController
 {
+    use UrlParam;
+    use FlashMensage;
+
     public function request(): void
     {
+        $pdo = Connection::startConn();
+        $table = filter_input(INPUT_POST,'table');
+        
+        $valuesParan = $this->getParamPost($table,$pdo);
+        $update = new Update($pdo,$table,$valuesParan);
+        $update->shine();
+        
+        if($update){
+            
+            header("Location:/view?table={$table}");
+            $this->showMensage('success','Adicionado com Sucesso');
+
+        }else{
+            
+            header("Location:/view?table={$table}");
+            $this->showMensage('danger','Erro ao Atualizar ! ');
+        }
         
     }
 }
